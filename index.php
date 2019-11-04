3b5bdbf40f3e1e7538b84635a9e39ecb3969b105
unknown <johnsonp6405@gmail.com>
2019-11-03 23:47:38 -0500

    Second COmmit

--- index.php
+++ index.php
@@ -1,3 +1,47 @@
-<?php
-echo "Hello World";
-?>
+<!DOCTYPE html>
+<html lang = "en">
+
+<head>
+<meta charset = "utf-8"/>
+<meta http-equiv = "X-UA-Compatible" content = "IE=edge"/>
+<meta name = "viewport" content = "width=device.width, intial-scale=1"/>
+<meta name = "Robots" content = "follow"/>
+
+<title>Login System</title>
+<!-- UIkit CSS -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/uikit@3.2.2/dist/css/uikit.min.css" />
+</head>
+
+<body>
+<div class = "uk-section uk-container uk-text-center">
+<div class="uk grid uk child width-1-3@s uk child width-1-1">
+	<form>
+    <fieldset class="uk-fieldset js-login">
+
+        <legend class="uk-legend">Legend</legend>
+
+        <div class="uk-margin">
+            <input class="uk-input" type="text" placeholder="Email">
+        </div>
+
+         <div class="uk-margin">
+            <input class="uk-input" type="text" placeholder="Password">
+        </div>
+
+         <div class="uk-margin">
+            <button class="uk-button uk-button-default" type="Submit">Login</button>
+        </div>
+    </fieldset>
+	</form>
+</div>
+</div>
+
+<!--jquery is required-->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+<!-- UIkit JS -->
+<script src="https://cdn.jsdelivr.net/npm/uikit@3.2.2/dist/js/uikit.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/uikit@3.2.2/dist/js/uikit-icons.min.js"></script>
+</body>
+
+</html>
