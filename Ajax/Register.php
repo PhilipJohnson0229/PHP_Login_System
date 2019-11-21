dc806043050da56b9363bb593229ad1d57d54dfe
unknown <johnsonp6405@gmail.com>
2019-11-21 01:48:42 -0500

    Great...more cool stuff

---
+++ Ajax/Register.php
@@ -0,0 +1,7 @@
+<?php
+//Allow this element to exist
+define('_CONFIG_', true);
+
+//Require this file to run the code below
+require_once "Inc/config.php"; 
+?>
---
+++ Inc/config.php
@@ -0,0 +1,13 @@
+<?php
+//if the constant _CONFIG_ is not defined, do not load this file
+if(!defined('_CONFIG_'))
+{
+	exit('you do not have a config file set up');
+}
+
+define('_ALLOW_FOOTER_', true);
+
+include_once("CLasses/DB.php");
+
+$con = DB::getConnection();
+?>
